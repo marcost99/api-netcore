9ed9e25023268f0f4e53fde234d911d598999887
Marcos Túlio <marcostuliio99@gmail.com>
2024-12-25 13:02:22 -0300

    creates migration to created a table tags

--- src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
@@ -8,5 +8,12 @@ namespace CashFlow.Infraestructure.DataAccess
         public CashFlowDbContext(DbContextOptions options) : base(options) {}
         public DbSet<Expense> Expenses { get; set; }
         public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<Tag>().ToTable("Tags");
+        }
     }
 }
--- src/CashFlow.Infraestructure/Migrations/20241225154845_TagsIntoExpenses.Designer.cs
+++ src/CashFlow.Infraestructure/Migrations/20241225154845_TagsIntoExpenses.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CashFlow.Infraestructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CashFlow.Infraestructure.Migrations
 {
     [DbContext(typeof(CashFlowDbContext))]
-    partial class CashFlowDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241225154845_TagsIntoExpenses")]
+    partial class TagsIntoExpenses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
